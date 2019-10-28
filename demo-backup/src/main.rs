66b4552f6613ed8d531cd390eed8b3e59a3d0d23
Mendelt Siebenga <msiebenga@gmail.com>
2019-10-28 13:52:21 +0100

    update demo-backup code with working aliasses and subscope example

--- demo-backup/src/main.rs
+++ demo-backup/src/main.rs
@@ -5,7 +5,7 @@ struct Application {}
 #[cmdr(help="This is an application that greets people")]
 impl Application {
     fn prompt(&self) -> String {
-        "%% ".to_string()
+        "%%".to_string()
     }
 
     #[cmd(hello)]
@@ -15,7 +15,7 @@ impl Application {
         CommandResult::Ok
     }
 
-    #[cmd(quit, help="help text\nquitter!")]
+    #[cmd(quit, help="help text\nquitter!", alias(exit, q, x))]
     fn quit_method(&self, _args: &[String]) -> CommandResult {
         CommandResult::Quit
     }
@@ -24,6 +24,27 @@ impl Application {
         println!("{} ??", command.command);
 
         CommandResult::Ok
+    }
+
+    #[cmd]
+    fn cd(&self, args: &[String]) -> CommandResult {
+        CommandResult::sub_scope(SubDir { name: args[0].clone()})
+    }
+}
+
+struct SubDir {
+    name: String
+}
+
+#[cmdr]
+impl SubDir {
+    fn prompt(&self) -> String {
+        self.name.clone()
+    }
+
+    #[cmd]
+    fn up(&self, _args: &[String]) -> CommandResult {
+        CommandResult::Exit
     }
 }
 
